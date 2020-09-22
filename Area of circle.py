d4275de20b0b8f7cd96d42720c4e822b7f06d2dc
kr230601 <71709979+kr230601@users.noreply.github.com>
2020-09-22 18:49:12 +0530

    Add files via upload
    These files prints the area of circle and extension respectively as per file name.

---
+++ Area of circle.py
@@ -0,0 +1,3 @@
+n=float(input("Input the radius of circle:: "))
+p=3.14*(n**2)
+print("The area of the circle with radius",n,"is:: ",p)
---
+++ Extension.py
@@ -0,0 +1,4 @@
+file=input("Enter the filename along with the extension followed by '.' :: ")
+spfile=file.split(".")
+extn=spfile[-1]
+print("The extension of the file is:: .",extn)
